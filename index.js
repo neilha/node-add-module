92a9cf14e8a8411d78bae100ea001e5f84ebf4df
Neil Harrold <neilha03@NHSNSSAZ.onmicrosoft.com>
2018-03-13 12:25:18 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Adds two numbers together
+ * @param {number} number1
+ * @param {number} number1
+ * @return {number}
+ */
+module.exports = function(number1, number2) {
+    return number1 + number2;
+};
